e9cca51c0f99715c4827bf814cfa0cc15ac8872a
LuizFernando110 <luizloona@gmail.com>
2025-05-06 10:04:25 -0300

    Feature: Campo de bimeste em nota, e atualizações no Serializer de estudante. Ademais, back configurado para permitir consumo do front.

--- media_escolar/media/models.py
+++ media_escolar/media/models.py
@@ -15,8 +15,8 @@ class Student(models.Model):
     
     def average_subject(self, subject):
         scores = self.score_set.select_related('subject').filter(subject=subject)
-        if scores.exists():
-            return sum(s.score for s in scores) / scores.count()
+        if scores.count() == 4:
+            return sum(s.score for s in scores) / 4
         return 0
 
 class Subject(models.Model):
@@ -26,9 +26,20 @@ class Subject(models.Model):
         return self.name
 
 class Score(models.Model):
+    BIMESTER_CHOICES = [
+        (1, '1º Bimestre'),
+        (2, '2º Bimestre'),
+        (3, '3º Bimestre'),
+        (4, '4º Bimestre'),
+    ]
+
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    term = models.PositiveSmallIntegerField(choices=BIMESTER_CHOICES, default=1)
     score = models.FloatField()
+
+    class Meta:
+        unique_together = ['student', 'subject', 'term']
 
     def __str__(self):
         return f"{self.student.name} - {self.subject.name} - {self.score}"
--- media_escolar/media/serializers.py
+++ media_escolar/media/serializers.py
@@ -1,26 +1,83 @@
 from .models import Student, Subject, Score
 from rest_framework import serializers
+from collections import defaultdict
+from django.db import IntegrityError
 
-class StudentSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Student
-        fields = ['name', 'average_overall']
-
-class SubjectSerializer(serializers.HyperlinkedModelSerializer):
+class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
-        fields = ['name']
+        fields = ['id','name']
 
-class ScoreSerializer(serializers.HyperlinkedModelSerializer):    
+class ScoreSerializer(serializers.ModelSerializer):    
     student_name = serializers.CharField(source='student.name', read_only=True)
     subject_name = serializers.CharField(source='subject.name', read_only=True)
 
     class Meta:
         model = Score
-        fields = ['student', 'subject', 'score', 'student_name', 'subject_name']
+        fields = ['id','student', 'subject', 'score', 'student_name', 'subject_name']
 
     def validate_score(self, value):
         if not 0 <= value <= 10:
             raise serializers.ValidationError('A nota deve estar entre 0 e 10')
         return value
+    
+    def validate(self, data):
+        student = data.get('student')
+        subject = data.get('subject')
+        term = data.get('term')
+
+        if Score.objects.filter(student=student, subject=subject, term=term).exists():
+            raise serializers.ValidationError('Já existe uma nota para esse aluno nesse bimestre')
         
+        return data
+    
+    def create(self, validated_data):
+        try:
+            score = Score.objects.create(**validated_data)
+            return score
+        except IntegrityError:
+            raise serializers.ValidationError('Já existe uma nota para esse aluno nesse bimestre')
+
+    def update(self, instance, validated_data):
+        instance.score = validated_data.get('score', instance.score)
+        instance.save()
+        return instance
+
+class StudentSerializer(serializers.ModelSerializer):
+    subjects = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Student
+        fields = ['id', 'name', 'average_overall', 'subjects']
+
+    def get_subjects(self, student):
+        data = defaultdict(list)
+
+        for score in student.score_set.select_related('subject'):
+            data[score.subject.name].append({
+                'term': score.term,
+                'score': score.score,
+            })
+
+        for scores in data.values():
+            scores.sort(key=lambda s: s['term'])
+
+        result = []
+        for subject_name, scores in data.items():
+            subject = Subject.objects.get(name=subject_name)
+            everage = student.average_subject(subject)
+            result.append({
+                'subject': subject_name,
+                'scores': scores,
+                'everage': everage
+            })
+        
+        return result
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if self.context.get('view').action == 'retrieve':
+            return representation
+        else:
+            representation.pop('subjects', None)
+            return representation
